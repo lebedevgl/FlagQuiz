410866e6022489bdd01047360805921109bade0c
Egor Lebedev <egor@abgcard.ru>
2016-11-11 18:19:51 +0300

    remove unused imports

--- app/src/main/java/com/deitel/flagquiz/SettingsActivity.java
+++ app/src/main/java/com/deitel/flagquiz/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.deitel.flagquiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
